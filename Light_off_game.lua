4dbf8e00a5b67a7cf8e841f6e6a97b364972c60f
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2024-02-25 18:28:26 +0900

    Update Light_off_game.lua

--- Light_off_game.lua
+++ Light_off_game.lua
@@ -8,56 +8,37 @@ local composer = require( "composer" )
 local scene = composer.newScene()
 
 function scene:create( event )
-local background = display.newImageRect("image/배경.png", display.contentWidth, display.contentHeight)
-	background.x, background.y = display.contentWidth/2, display.contentHeight/2
-
 	--- 타이머 추가 -----------
 	local time= display.newText(75, display.contentWidth*0.37, display.contentHeight*0.1)
  	time.size = 100
  	time:setFillColor(0)
 
+	-----배경-----
+	local background = display.newRoundedRect(display.contentWidth/2, display.contentHeight/2, display.contentWidth*0.85, display.contentHeight*0.75, 55)
+    background.strokeWidth = 10
+    background:setStrokeColor( 0.6 )
+    background:setFillColor(1, 1, 0.9 )
+
+    local title = display.newText("전기 스위치를 꺼 주세요!", display.contentWidth/2, display.contentHeight*0.2)
+    title:setFillColor( 0 )
+    title.size = 70
+
  	---스위치----------
- 	local switchOn = display.newImage("image/스위치1.png")
+ 	local switchOn = display.newImage("image/퀘스트_자전거_스위치/스위치1.png")
  	switchOn.height = 500
  	switchOn.width = 350
  	switchOn.x, switchOn.y = display.contentHeight*0.9, display.contentWidth*0.3
 
  	function tapEvent1( event )
- 		local switchOff = display.newImage("image/스위치2.png")
+ 		local switchOff = display.newImage("image/퀘스트_자전거_스위치/스위치2.png")
  		switchOff.height = 500
  		switchOff.width = 350
  		switchOff.x, switchOff.y = display.contentHeight*0.9, display.contentWidth*0.3
  	end
  	switchOn:addEventListener("tap", tapEvent1)
-end
 
 
-function scene:show( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
-	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
-	end	
-end
 
-function scene:hide( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		--
-		-- INSERT code here to pause the scene
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
-	elseif phase == "did" then
-		-- Called when the scene is now off screen
-	end
 end
 
 function scene:destroy( event )
@@ -73,8 +54,6 @@ end
 
 -- Listener setup
 scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
 
 -----------------------------------------------------------------------------------------
