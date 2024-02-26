530fccc3cd06a2f7d8f0f074674b50d376fe75ff
madisonyoon <kate2016@naver.com>
2024-02-26 12:06:56 +0900

    quest_turnoff_faucet.lua

--- quest_turnoff_faucet.lua
+++ quest_turnoff_faucet.lua
@@ -8,6 +8,16 @@ local faucetImages = {
 local currentImageIndex = 1
 local tapCount = 0
 
+--배경
+local background = display.newRoundedRect(display.contentWidth/2, display.contentHeight/2, display.contentWidth*0.85, display.contentHeight*0.75, 55)
+    background.strokeWidth = 10
+    background:setStrokeColor( 0.6 )
+    background:setFillColor(1, 1, 0.9 )
+
+    local title = display.newText("핸들을 클릭해 물을 멈춰주세요!", display.contentWidth/2, display.contentHeight*0.2)
+    title:setFillColor( 0 )
+    title.size = 70
+
 -- 이미지 로드 함수
 local function loadImages()
     -- 이미지 로드 코드
@@ -44,7 +54,7 @@ end
 
 -- 사용자 입력 처리
 while true do
-    print("탭을 입력하세요.")
+    print("핸들을 클릭해주세요.")
     io.read()  -- 사용자 입력 대기
     processInput()  -- 입력 처리
 end
