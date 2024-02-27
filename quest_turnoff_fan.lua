84c0c69d4df4a29d11ad6ad9a9da7472ef1bd3ff
서영 <134121471+gooorm@users.noreply.github.com>
2024-02-27 14:45:27 +0900

    버튼 선행조건 수정

--- quest_turnoff_fan.lua
+++ quest_turnoff_fan.lua
@@ -1,3 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- 선풍기 끄기.lua
+--
+-----------------------------------------------------------------------------------------
+
 local composer = require( "composer" )
 local scene = composer.newScene()
 
@@ -21,47 +27,43 @@ function scene:create( event )
     local button2 = display.newRect(display.contentCenterX, display.contentCenterY, 80, 30)
     button2.x = button2.x + 210
     button2.y = button2.y + 70
-    button2.enabled = false
     button2:setFillColor(0,0,1)
 
-
-    local button3 = display.newRect(display.contentCenterX, display.contentCenterY, 80, 50)
+    local button3 = display.newRect(display.contentCenterX, display.contentCenterY, 80, 70)
     button3.x = button3.x + 210
     button3.y = button3.y + 120
     button3:setFillColor(0,0,1)
         print("button2 enabled")
-    button3.enabled = false
+
+    local b1 = false;
+    local b2 = false;
+    --local b3 = false;
 
     local fan = display.newImage("image/선풍기/선풍기1.png")
     fan.height = 500
     fan.width = 520
     fan.x, fan.y = display.contentHeight*0.9, display.contentWidth*0.3
 
-    print(button2.enabled)
 
-
-    local function tapEvent1( event )
-        fan:removeSelf()
-        fan = display.newImage("image/선풍기/선풍기2.png")
-        fan.height = 500
-        fan.width = 520
-        fan.x, fan.y = display.contentHeight*0.9, display.contentWidth*0.3
-        print(button2.enabled)
-        button2.enabled = true
-        print("button2 enabled")
+    local function tapEvent1( event )        
+            fan:removeSelf()
+            fan = display.newImage("image/선풍기/선풍기2.png")
+            fan.height = 500
+            fan.width = 520
+            fan.x, fan.y = display.contentHeight*0.9, display.contentWidth*0.3
+            b1 = true
     end
     button1:addEventListener("tap", tapEvent1)
 
     local function tapEvent2( event )
+        if b1 == true then
         fan:removeSelf()
         fan = display.newImage("image/선풍기/선풍기3.png")
         fan.height = 500
         fan.width = 520
         fan.x, fan.y = display.contentHeight*0.9, display.contentWidth*0.3
-        button3.enabled = true
-    print(button2.enabled)
-
-        print("button3 enabled")
+        b2 = true
+        end
     end
     button2:addEventListener("tap", tapEvent2)
 
@@ -79,12 +81,14 @@ function scene:create( event )
     end
 
     local function tapEvent3( event )
+        if b2 == true then
         local timeAttack = timer.performWithDelay(1000, questEnd)
         fan:removeSelf()
         fan = display.newImage("image/선풍기/선풍기4.png")
         fan.height = 500
         fan.width = 520
         fan.x, fan.y = display.contentHeight*0.9, display.contentWidth*0.3
+        end
     end
     button3:addEventListener("tap", tapEvent3)
 end
