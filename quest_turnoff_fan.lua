717c10d36500700409581f33dd0112279fcbac13
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2024-02-27 11:02:38 +0900

    Update quest_turnoff_fan.lua

--- quest_turnoff_fan.lua
+++ quest_turnoff_fan.lua
@@ -20,40 +20,51 @@ function scene:create( event )
     title.size = 70
 
     ---선풍기 끄기 퀘스트----------
+    local button1 = display.newRect(display.contentCenterX, display.contentCenterY, 30, 30)
+    button1.x = button1.x - 95
+    button1.y = button1.y + 240
+
+    local button2 = display.newRect(display.contentCenterX, display.contentCenterY, 80, 50)
+    button2.x = button2.x + 210
+    button2.y = button2.y + 70
+
+    local button3 = display.newRect(display.contentCenterX, display.contentCenterY, 80, 50)
+    button3.x = button3.x + 210
+    button3.y = button3.y + 120
+
     local fan1 = display.newImage("image/선풍기/선풍기1.png")
     fan1.height = 500
     fan1.width = 520
     fan1.x, fan1.y = display.contentHeight*0.9, display.contentWidth*0.3
-    fan2 = display.newImage("image/선풍기/선풍기2.png")
-    fan2.height = 0
-    fan2.width = 0
-    fan3 = display.newImage("image/선풍기/선풍기3.png")
-    fan3.height = 0
-    fan3.width = 0
-    fan4 = display.newImage("image/선풍기/선풍기4.png")
-    fan4.height = 0
-    fan4.width = 0
 
     local function tapEvent1( event )
+        fan2 = display.newImage("image/선풍기/선풍기2.png")
         fan2.height = 500
         fan2.width = 520
         fan2.x, fan2.y = display.contentHeight*0.9, display.contentWidth*0.3
         display.remove(fan1)
     end
-    fan1:addEventListener("tap", tapEvent1)
+    button1:addEventListener("tap", tapEvent1)
 
     local function tapEvent2( event )
+        fan3 = display.newImage("image/선풍기/선풍기3.png")
         fan3.height = 500
         fan3.width = 520
         fan3.x, fan3.y = display.contentHeight*0.9, display.contentWidth*0.3
         display.remove(fan2)
     end
-    fan2:addEventListener("tap", tapEvent2)
+    button2:addEventListener("tap", tapEvent2)
 
-    local function questEnd(event)
+    function questEnd(event)
         display.remove(background)
         display.remove(title)
+        display.remove(fan1)
+        display.remove(fan2)
+        display.remove(fan3)
         display.remove(fan4)
+        display.remove(button1)
+        display.remove(button2)
+        display.remove(button3)
 
         scene:destroy()
         composer.removeScene("quest_turnoff_fan")
@@ -62,13 +73,13 @@ function scene:create( event )
 
     local function tapEvent3( event )
         local timeAttack = timer.performWithDelay(1000, questEnd)
+        fan4 = display.newImage("image/선풍기/선풍기4.png")
         fan4.height = 500
         fan4.width = 520
         fan4.x, fan4.y = display.contentHeight*0.9, display.contentWidth*0.3
         display.remove(fan3)
-
     end
-    fan3:addEventListener("tap", tapEvent3)
+    button3:addEventListener("tap", tapEvent3)
 end
 
 function scene:destroy( event )
